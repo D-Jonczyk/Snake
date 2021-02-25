a9128eeaaed1203cf09d1eb51ee88a55c33beca0
Daniel Jonczyk <danieljonczyk.w@gmail.com>
2021-02-25 10:07:26 +0100

    refactoring main

--- snake.c
+++ snake.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <stdbool.h>  
 #include <time.h>
-#define map_size_x 15
-#define map_size_y 25
+
+#include "dialogs.h"
+#include "food_point.h"
 
 //    SNAKE NA PODSTAWIE LISTY DWUKIERUNKOWEJ
 
@@ -16,32 +13,6 @@
 	-jakies podpowiedzi co do klawiszy
 */
 
-HANDLE wHnd;
-
-//poziomy trudnosci
-short int easy = 18;
-short int medium = 14;
-short int hard = 10;
-
-struct point{
-	int x,y;
-};
-
-struct node{
-	struct node *prev;
-	struct node *next;
-	int x,y, _x, _y;
-};
-
-struct snake{
-	struct node *head;
-	struct node *tail;
-	long int score;
-	short int count,last_x,last_y;
-	bool field[map_size_x][map_size_y]; //miejsca w ktorych aktualnie znajduje sie waz
-	bool status; //status gry(false=game over)
-};
-
 void board_init(char (*ptr)[map_size_y]);
 void board_print(char (*ptr)[map_size_y], COORD begin);
 void wait(short int);
@@ -52,19 +23,11 @@ void snake_set(char (*ptr)[map_size_y], struct snake *s);
 void movement(char (*ptr)[map_size_y], struct snake *s, int move_x, int move_y);
 void append(struct snake *s, struct point *p, short int difficulty);
 
-void random_point(struct snake *s, struct point *p, char (*ptr)[map_size_y]);
-bool compare_point(struct snake *s, struct point *p);
-void set_point(struct point *p, char (*ptr)[map_size_y]);
-
-void menu(void);
-char menu_option(void);
-char menu_difficulty(void);
-
 int main() {
 	COORD begin;
 	begin.X=0;
 	begin.Y=0;
-	
+
 	srand(time(NULL));
 	short int difficulty;
 	difficulty = easy;
@@ -72,13 +35,13 @@ int main() {
 	char choice_d,choice,last;
 	char tab[map_size_x][map_size_y];
 	char (*t_ptr)[map_size_y]= tab;
-	
+
 	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
-	
+
 	SetConsoleTitle(TEXT("Snake v1.0"));
 	SMALL_RECT windowSize = {0, 0, 25, 17};
 	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
-    
+
  	// zmienna COORD trzymajaca rozmiar bufora:
 	COORD bufferSize = {26, 18};
   	// zmiana rozmiarow bufora:
@@ -118,7 +81,7 @@ int main() {
 				return 0;
 				break;
 		}
-		
+
 		struct snake *snake_ptr = (struct snake*)malloc(sizeof(struct snake));
 		struct point *point_ptr = (struct point*)malloc(sizeof(struct point));
 		board_init(t_ptr);
@@ -126,9 +89,9 @@ int main() {
 		snake_set(t_ptr,snake_ptr);
 		random_point(snake_ptr,point_ptr, t_ptr);
 		set_point(point_ptr, t_ptr);
-		board_print(t_ptr, begin);	
+		board_print(t_ptr, begin);
 		ch = 0;
-		
+
 		while(!snake_ptr->status) //glowna petla
 		{
 			if( kbhit() )
@@ -142,11 +105,11 @@ int main() {
 							ch = getch();
 					}while(ch!=112);
 				}
-				
+
 				if((ch != 80 && ch!=72&&ch!=75&&ch!=77) || abs(ch-last)==8 || abs(ch-last)==2) //((zly klawisz) || zmiana kierunku o 180* || zmiana kierunku o 180*)
 					ch = last;
 			}
-	
+
 			switch(ch)
 			{
 				case 80: movement(t_ptr, snake_ptr, 1, 0);	break;  //down
@@ -160,13 +123,13 @@ int main() {
 			if( compare_point(snake_ptr, point_ptr) )
 			{
 				Beep(300,30);
-				append(snake_ptr,point_ptr,difficulty);	
+				append(snake_ptr,point_ptr,difficulty);
 				random_point(snake_ptr,point_ptr, t_ptr);
 				set_point(point_ptr, t_ptr);
 			}
-			
+
 			wait(difficulty);
-			
+
 			if( kbhit() )
 			{
 				last = ch;
@@ -181,9 +144,9 @@ int main() {
 				if((ch != 80 && ch!=72&&ch!=75&&ch!=77) || abs(ch-last)==8 || abs(ch-last)==2) //((zly klawisz) || zmiana kierunku o 180* || zmiana kierunku o 180*)
 					ch = last;
 			}
-		printf("Wynik: %d\t\n", snake_ptr->score);
+		printf("Wynik: %ld\t\n", snake_ptr->score);
 		} //koniec petli gry
-		
+
 		SetConsoleCursorPosition(wHnd,game_over);
 		printf("Przegrales!\n");
 		free(snake_ptr);
@@ -194,7 +157,7 @@ int main() {
 void board_init(char (*ptr)[map_size_y]) //wypelnienie tablicy(mapy)
 {
 	int i,j;
-	
+
 	for(i=0;i<map_size_x;i++)
 	{
 		for(j=0;j<map_size_y;j++)
@@ -205,7 +168,7 @@ void board_init(char (*ptr)[map_size_y]) //wypelnienie tablicy(mapy)
 				ptr[i][j] = 186;
 			else
 				ptr[i][j] = 255; //srodek mapy(pusty znak)
-		
+
 		//corners
 			if( i==0 && j==0 )
 				ptr[i][j] = 201;
@@ -237,7 +200,7 @@ void snake_init(struct snake *s) //chyba mozna to bylo krocej napisac ale boje s
 	s->count = 1;
 	s->score = 1;
 	s->status = false;
-	
+
 	for(i=0;i<map_size_x;i++)
 		for(j=0;j<map_size_y;j++)
 			s->field[i][j] = false;
@@ -250,7 +213,7 @@ void board_print(char (*ptr)[map_size_y], COORD begin)
 	SetConsoleCursorPosition(wHnd,begin);
 	for(i=0;i<map_size_x;i++){
 		for(j=0;j<map_size_y;j++)
-			printf("%c", ptr[i][j]);	
+			printf("%c", ptr[i][j]);
 		puts("");
 	}
 }
@@ -258,7 +221,7 @@ void board_print(char (*ptr)[map_size_y], COORD begin)
 void wait(short int difficulty)
 {
     clock_t koniec = clock() + difficulty * CLOCKS_PER_SEC/100;
-    while( clock() < koniec ) continue;  
+    while( clock() < koniec ) continue;
 }
 
 void snake_set(char (*ptr)[map_size_y], struct snake *s) //ustawia glowke na poczatkowe koordynaty
@@ -280,7 +243,7 @@ void movement(char (*ptr)[map_size_y], struct snake *s, int move_x, int move_y) 
 		element->_x += move_x;
 		element->_y += move_y;
 		do
-		{	
+		{
 			ptr[element->_x][element->_y] = 178;
 			//gdy waz ma jeden element
 			if(s->tail==NULL)
@@ -308,14 +271,14 @@ void movement(char (*ptr)[map_size_y], struct snake *s, int move_x, int move_y) 
 					ptr[s->last_x][s->last_y] = 0;
 				}
 				else{
-		
+
 					element->_x = element->next->x;
 					element->_y = element->next->y;
 					element->next->x = element->next->_x;
 					element->next->y = element->next->_y;
 				}
 			}
-			
+
 			if(s->tail)
 				if( (s->head->_x==element->_x && s->head->_y==element->_y) ) //kolizja z wezem
 				{
@@ -324,29 +287,9 @@ void movement(char (*ptr)[map_size_y], struct snake *s, int move_x, int move_y) 
 				}
 			s->field[element->_x][element->_y] = true;
 			s->field[s->last_x][s->last_y] = false;
-			
+
 		}while(element->prev);
 	}
-}
-
-void random_point(struct snake *s, struct point *p, char (*ptr)[map_size_y]) //randomuje punkt i sprawdza czy pojawil sie na wezu
-{
-	p->x = ( rand() % (map_size_x-2) ) + 1;
-	p->y = ( rand() % (map_size_y-2) ) + 1;
-	if(s->field[p->x][p->y] )
-		random_point(s,p,ptr);
-}
-
-void set_point(struct point *p, char (*ptr)[map_size_y])
-{
-	ptr[p->x][p->y] = 158;
-}
-
-bool compare_point(struct snake *s, struct point *p)
-{
-	if(s->head->_x == p->x && s->head->_y == p->y)
-		return true;
-	return false;
 }
 
 void append(struct snake *s, struct point *p, short int difficulty) //dodaje element na koniec weza
@@ -368,9 +311,9 @@ void append(struct snake *s, struct point *p, short int difficulty) //dodaje ele
 		s->tail->prev = element;
 		s->tail = element;
 	}
-	
+
 	s->count++;
-	
+
 	if(difficulty==easy)
 		s->score += (s->count*0.4) * (difficulty*0.1);
 	else if(difficulty==medium)
@@ -379,40 +322,4 @@ void append(struct snake *s, struct point *p, short int difficulty) //dodaje ele
 		s->score += (s->count*0.4) * (difficulty*0.4);
 }
 
-void menu(void)
-{
-	SetConsoleTextAttribute(wHnd, BACKGROUND_GREEN | BACKGROUND_RED  | FOREGROUND_RED);
-	printf("\n\n\n");
-	printf("-------------------------\n");
-	printf("[1] Nowa gra\t\t\n");
-	printf("[2] Poziom trudnosci\t\n");
-	printf("[3] Wyjscie\t\t\n");
-	printf("-------------------------\n");
-}
 
-char menu_option(void)
-{
-	char choice;
-	do{
-		choice = getch();
-		switch( choice )
-		{
-			case '1':
-			case '2':
-			case '3': return(choice);
-			default:
-				choice='0';
-		}
-	}while( choice == '0');
-	return choice;
-}
-
-char menu_difficulty(void)
-{
-	printf("\n");
-	printf("-------------------------\n");
-	printf("[1] Latwy\t\t\n");
-	printf("[2] Sredni\t\n");
-	printf("[3] Trudny\t\t\n");
-	printf("-------------------------\n");
-}
