f9ff7442f3da748aa1a0dd80d6e7ec26b460cf45
Daniel Jonczyk <danieljonczyk.w@gmail.com>
2021-02-27 23:15:20 +0100

    but hey, it works, and i made the code somewhat readable

--- dialogs.c
+++ dialogs.c
@@ -4,40 +4,40 @@ extern int difficulty;
 
 char getUserMenuChoice(void)
 {
-	char choice;
-	do{
-		choice = getch();
-		switch( choice )
-		{
-			case '1':
-			case '2':
-			case '3': return(choice);
-			default:
-				choice='0';
-		}
-	}while( choice == '0');
-	return choice;
+    char choice;
+    do {
+        choice = getch();
+        switch (choice) {
+        case '1':
+        case '2':
+        case '3':
+            return (choice);
+        default:
+            choice = '0';
+        }
+    } while (choice == '0');
+    return choice;
 }
 
 void printMainMenu(void)
 {
-	SetConsoleTextAttribute(wHnd, BACKGROUND_GREEN | BACKGROUND_RED  | FOREGROUND_RED);
-	printf("\n\n");
-	printf("-------------------------\n");
-	printf("[1] New Game\t\t\n");
-	printf("[2] Difficulty\t\n");
-	printf("[3] Exit\t\t\n");
-	printf("-------------------------\n");
+    SetConsoleTextAttribute(wHnd, BACKGROUND_GREEN | BACKGROUND_RED | FOREGROUND_RED);
+    printf("\n\n");
+    printf("-------------------------\n");
+    printf("[1] New Game\t\t\n");
+    printf("[2] Difficulty\t\n");
+    printf("[3] Exit\t\t\n");
+    printf("-------------------------\n");
 }
 
 void printDifficultyMenu(void)
 {
-	printf("\n");
-	printf("-------------------------\n");
-	printf("[1] Easy\t\t\n");
-	printf("[2] Medium\t\n");
-	printf("[3] Hard\t\t\n");
-	printf("-------------------------\n");
+    printf("\n");
+    printf("-------------------------\n");
+    printf("[1] Easy\t\t\n");
+    printf("[2] Medium\t\n");
+    printf("[3] Hard\t\t\n");
+    printf("-------------------------\n");
 }
 
 void processUserChoice()
--- food_point.c
+++ food_point.c
@@ -1,25 +1,26 @@
 #include "food_point.h"
 
-void randomFoodPoint(struct snake *s, struct point *p, char (*ptr)[map_size_y])
+void randomFoodPoint(struct snake* s, struct point* p, char (*ptr)[map_size_y])
 {
-	p->x = ( rand() % (map_size_x-2) ) + 1;
-	p->y = ( rand() % (map_size_y-2) ) + 1;
-	if(s->fieldsOccupiedBySnake[p->x][p->y] ) //checks if point spawned at snake's actual position
-		randomFoodPoint(s,p,ptr);
+    p->x = (rand() % (map_size_x - 2)) + 1;
+    p->y = (rand() % (map_size_y - 2)) + 1;
+    if (s->fieldsOccupiedBySnake[p->x][p->y]) //checks if point spawned at snake's actual position
+        randomFoodPoint(s, p, ptr);
 }
 
-void printFoodPoint(struct point *p, char (*ptr)[map_size_y])
+void printFoodPoint(struct point* p, char (*ptr)[map_size_y])
 {
-	ptr[p->x][p->y] = FOOD_TEXTURE;
+    ptr[p->x][p->y] = FOOD_TEXTURE;
 }
 
-bool scoredPoint(struct snake *s, struct point *p)
+bool scoredPoint(struct snake* s, struct point* p)
 {
-	if(s->head->newX == p->x && s->head->newY == p->y)
-		return true;
-	return false;
+    if (s->head->newX == p->x && s->head->newY == p->y)
+        return true;
+    return false;
 }
 
-void pointScoredSound(){
-    Beep(300,30);
+void pointScoredSound()
+{
+    Beep(300, 30);
 }
