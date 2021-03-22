c2a2369baaf71b94a8dc9380816e2ccafd570ba3
Daniel Jończyk <66755218+D-Jonczyk@users.noreply.github.com>
2021-03-22 10:50:50 +0100

    comment update

--- food_point.c
+++ food_point.c
@@ -4,7 +4,7 @@ void randomFoodPoint(struct snake* s, struct point* p, char (*ptr)[map_size_y])
 {
     p->x = (rand() % (map_size_x - 2)) + 1;
     p->y = (rand() % (map_size_y - 2)) + 1;
-    if (s->fieldsOccupiedBySnake[p->x][p->y]) //checks if point spawned at snake's actual position
+    if (s->fieldsOccupiedBySnake[p->x][p->y]) //checks if point has spawned at snake's actual position
         randomFoodPoint(s, p, ptr);
 }
 
